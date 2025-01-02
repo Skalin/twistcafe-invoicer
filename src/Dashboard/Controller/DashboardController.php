8e542f5666381fa4a36b231795558cba41b126a5
Dominik Skála <dominikskala@seznam.cz>
2025-01-02 10:55:12 +0100

    feat: add dashboard module

--- src/Dashboard/Controller/DashboardController.php
+++ src/Dashboard/Controller/DashboardController.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-
 namespace App\Dashboard\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,7 +11,6 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('dashboard', name: 'dashboard', methods: ['GET'])]
 class DashboardController extends AbstractController
 {
-
     public function __invoke(): Response
     {
         return $this->render(
